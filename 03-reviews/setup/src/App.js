73a5d2a995e74799eada6ef332e3e70a6e838ce7
sehriban matur <sehribanmatur9@gmail.com>
2022-02-09 14:01:12 -0500

    random prev-next person

--- 03-reviews/setup/src/App.js
+++ 03-reviews/setup/src/App.js
@@ -1,7 +1,19 @@
 import React from 'react';
+
 import Review from './Review';
+import {BsBatteryCharging} from "react-icons/bs"
 function App() {
-  return <h2>reviews project setup</h2>;
+  return <main>
+<section className='container'>
+  <div className='title'>
+    <h2> Our reviews     </h2>
+    <div className='underline'></div>
+  </div>
+  <Review/>
+</section>
+  
+ 
+  </main>
 }
 
 export default App;
